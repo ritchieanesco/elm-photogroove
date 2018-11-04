be48af0176d258290c8b44412b6fd60622c90918
ritchieanesco <info@ritchieanesco.com>
2018-11-04 18:59:48 +1100

    - Highlight "selected thumbnail - Display large image of selected thumbnail

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -29,19 +29,48 @@ urlPrefix =
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ]
+            (List.map (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
         ]
 
 
-viewThumbnail thumbnail =
-    img [ src (urlPrefix ++ thumbnail.url) ] []
+
+{-
+   Use anonymous function to pass selected url and photo data
+-}
+
+
+viewThumbnail selectedUrl thumbnail =
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        ]
+        []
+
+
+
+{-
+   Use Html.classList to determine if thumbnail is selected
+   classList uses a list of tuples, first providing classname
+   and the condition for that classname
+-}
 
 
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "3.jpeg"
+    }
 
 
 main =
